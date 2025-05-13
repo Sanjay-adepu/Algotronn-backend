7557a9e9273688ce666a14dc8a4165abdf2c8b76
Sanjay-adepu <adepusanjay812@gmail.com>
2025-05-13 10:52:02 +0530

    Update User.js

--- Model/User.js
+++ Model/User.js
@@ -1,1 +1,14 @@
 
+// Model.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  googleId: { type: String, required: true, unique: true },
+  name: String,
+  email: { type: String, required: true, unique: true },
+  picture: String,
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
