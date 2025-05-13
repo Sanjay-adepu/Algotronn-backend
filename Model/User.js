16f11456ca1cd67b85f2b286249387f28e79d763
Sanjay-adepu <adepusanjay812@gmail.com>
2025-05-13 16:03:13 +0530

    Update User.js

--- Model/User.js
+++ Model/User.js
@@ -1,11 +1,12 @@
-// Model/User.js
 const mongoose = require('mongoose');
+const Address = require('./addressModel');  // Import Address model
 
 const userSchema = new mongoose.Schema({
   googleId: { type: String, required: true, unique: true },
   name: String,
   email: { type: String, required: true, unique: true },
   picture: String,
+  address: { type: mongoose.Schema.Types.ObjectId, ref: 'Address' },  // Reference to Address model
 }, { timestamps: true });
 
 module.exports = mongoose.models.User || mongoose.model('User', userSchema);
