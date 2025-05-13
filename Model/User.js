771011d7b9c95ecf080011f294e570d13396e1e9
Sanjay-adepu <adepusanjay812@gmail.com>
2025-05-13 17:06:36 +0530

    Update User.js

--- Model/User.js
+++ Model/User.js
@@ -1,12 +1,24 @@
+// User.js
 const mongoose = require('mongoose');
-const Address = require('./addressModel.js');  // Import Address model
+
+const addressSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  mobile: { type: String, required: true },
+  email: { type: String, required: true },
+  address: { type: String, required: true },
+  locality: { type: String, default: '' },
+  landmark: { type: String, default: '' },
+  pincode: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+}, { _id: false });  // Disable _id for subdocument if not needed
 
 const userSchema = new mongoose.Schema({
   googleId: { type: String, required: true, unique: true },
   name: String,
   email: { type: String, required: true, unique: true },
   picture: String,
-  address: { type: mongoose.Schema.Types.ObjectId, ref: 'Address' },  // Reference to Address model
+  address: addressSchema, // Embedded address
 }, { timestamps: true });
 
 module.exports = mongoose.models.User || mongoose.model('User', userSchema);
