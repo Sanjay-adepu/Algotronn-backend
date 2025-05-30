bc053aec6241334cd23155dc37a0a6e0adf0703f
Sanjay-adepu <adepusanjay812@gmail.com>
2025-05-30 09:39:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,8 +44,8 @@ const client = new OAuth2Client("741240365062-r2te32gvukmekm4r55l4ishc0mhsk4f9.a
 const app = express();
 app.use(bodyParser.json());
 app.use(cors({
-  origin: ["https://algotronn.vercel.app", "http://localhost:5173","https://algo-admin-one.vercel.app"],
-  methods: ["GET", "POST"]
+  origin: ["https://algotronn.vercel.app", "http://localhost:5173", "https://algo-admin-one.vercel.app"],
+  methods: ["GET", "POST", "DELETE"]
 }));
 
 let isConnected = false;
