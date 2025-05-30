40b9c98a3cde2b17210fa5a459cac7b043351626
Sanjay-adepu <adepusanjay812@gmail.com>
2025-05-30 06:53:33 +0530

    Create Product.js

---
+++ Model/Product.js
@@ -0,0 +1,17 @@
+// Mongoose Schema for Product
+const productSchema = new mongoose.Schema({
+name: String,
+imageUrl: String,
+description: String,
+type: { type: String, enum: ['public', 'duplicate'], required: true },
+stock: Boolean,
+// For 'duplicate'
+price: Number,
+originalPrice: Number,
+discount: Number,
+// For 'public'
+summary: String,
+dailyPL: String,
+publicType: String
+});
+const Product = mongoose.model("Product", productSchema);
