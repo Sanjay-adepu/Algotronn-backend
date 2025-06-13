940080712da8e2b1eeddf7c843f369a179d42669
Sanjay-adepu <adepusanjay812@gmail.com>
2025-06-13 09:58:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ const client = new OAuth2Client("741240365062-r2te32gvukmekm4r55l4ishc0mhsk4f9.a
 const app = express();
 app.use(bodyParser.json());
 app.use(cors({
-  origin: ["https://algobazarr.vercel.app", "http://localhost:5173", "https://algobazarr-admin.vercel.app"],
+  origin: ["https://algobazarr.vercel.app", "https://www.algobazarr.store", "https://algobazarr-admin.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
